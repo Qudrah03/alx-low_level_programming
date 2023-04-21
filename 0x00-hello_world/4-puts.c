484bd984068f902dfe5d47b583dc68bcb43b5963
Qudrah03 <kudirattosin03@gmail.com>
2023-04-21 09:47:25 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always o 
+ * Return: Always 0 (Success) 
  */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-	return (o);
+	return (0);
 }
