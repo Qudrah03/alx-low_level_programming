63c5115980c13253558605d65e380252a2e44683
Qudrah03 <kudirattosin03@gmail.com>
2023-05-03 04:23:42 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unlstd.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * 0c: char to be printed
+ * Return: On success 1
+ */
+int _putchar (char c)
+{
+	return (write(1, &c, 1)):
+}
+
+
